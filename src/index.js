2c7c0f1fb919c1e93ea4b7f4ef7d9fd5198cfd32
root <root@vmi325994.contaboserver.net>
2020-03-17 04:38:55 +0100

    Primera seccion

---
+++ src/index.js
@@ -0,0 +1,47 @@
+var http = require("http");
+var url = require("url");
+var querystring = require("querystring");
+var log = require('./modules/mylog');
+var { countries } = require("countries-list");
+
+var server = http.createServer(function(request, response) {
+	var parsed = url.parse(request.url);
+	console.log("parsed:", parsed);
+
+	var pathname = parsed.pathname;
+	var query = querystring.parse(parsed.query);
+	console.log("query: ", query);
+
+	if (pathname  === "/"){
+		response.writeHead(200,{'Content-Type': 'text/html'});	
+		response.write('<html><body><p>Home<p></body></html>');
+		response.end();
+	}else if(pathname === "/exit"){
+		response.writeHead(200,{'Content-Type': 'text/html'});	
+		response.write('<html><body><p>exit</p></body></html>');
+		response.end();
+	}else if(pathname === "/country"){
+		var result = log.info(request.url);
+		response.writeHead(200,{'Content-Type': 'text/html'});	
+		response.write(JSON.stringify(countries[query.code]));
+		response.end();
+	}else if(pathname === "/info"){
+		var result = log.info(request.url);
+		response.writeHead(200,{'Content-Type': 'text/html'});	
+		response.write(result);
+		response.end();
+	}else if(pathname === "/error"){
+		var result = log.error(request.url);
+		response.writeHead(200,{'Content-Type': 'text/html'});	
+		response.write(result);
+		response.end();
+	}else{
+		response.writeHead(404,{'Content-Type': 'text/html'});	
+		response.write('<html><body><p>NOT FOUND</p></body></html>')
+		response.end();
+	}
+});
+server.listen(4000);
+
+console.log("running in port 4000");
+
