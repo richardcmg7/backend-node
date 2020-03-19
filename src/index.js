ee5ed66bc988075dd38c1abe07d3cfda3a9398ab
root <root@vmi325994.contaboserver.net>
2020-03-19 03:32:02 +0100

    Definiendo rutas en archivo diferente

--- src/index.js
+++ src/index.js
@@ -1,55 +1,11 @@
-const express = require("express");
-
-var log = require('./modules/mylog');
-var { countries } = require("countries-list");
-
+const express = require('express');
+const routes = require('./routes/index');
 const app = express();
-app.get('/', function(request, response){
-	response.status(200).send("HELLO");
-});
-app.get('/info', function (request, response) {
-    log.info('info nodemon');
-    response.send('info 2');
-})
-app.get('*', function (request, response) {
-    response.status(404).send("NOT FOUND");
-    
-})
-//var server = http.createServer(function(request, response){ 
-//var parsed = url.parse(request.url);
-//console.log("parsed:", parsed);
-//var pathname = parsed.pathname;
-//var query = querystring.parse(parsed.query);
-//	console.log("query: ", query);
-//	response.writeHead(200,{'Content-Type': 'text/html'});			response.write('<html><body><p>Home<p></body></html>');
-//	response.end();
-//}else if(pathname === "/exit"){
-//	response.writeHead(200,{'Content-Type': 'text/html'});	
-//	response.write('<html><body><p>exit</p></body></html>');
-//	response.end();
-//}else if(pathname === "/country"){
-//	var result = log.info(request.url);
-//	response.writeHead(200,{'Content-Type': 'text/html'});	
-//	response.write(JSON.stringify(countries[query.code]));
-///	response.end();
-///}else if(pathname === "/info"){
-///	var result = log.info(request.url);
-///	response.writeHead(200,{'Content-Type': 'text/html'});	
-///	response.write(result);
-///	response.end();
-///}else if(pathname === "/error"){
-///	var result = log.error(request.url);
-///	response.writeHead(200,{'Content-Type': 'text/html'});	
-///	response.write(result);
-///	response.end();
-///}else{
-///	response.writeHead(404,{'Content-Type': 'text/html'});	
-///	response.write('<html><body><p>NOT FOUND</p></body></html>')
-///	response.end();
-///}
-///
-///});
-app.listen(4000,function(){	
+
+routes(app);
+
+
+app.listen(4000,() => {	
 	console.log("running in port 4000");
 });
 
--- src/modules/mylog.js
+++ src/modules/mylog.js
@@ -1,11 +1,13 @@
-function info(text){
+const info = text => {
 	console.log("INFO: ", text);
 	return text;
 }
-function error(text){
+const error = text => {
 	console.log("ERROR: ", text);
 	return text;
 }
 
 module.exports = {info, error};
+module.exports.info = info;
+module.exports.error = error;
 
